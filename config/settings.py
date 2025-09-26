a6e3fb109b49f306d4ffeb829171e10ecc678396
minju123486 <minju123486@gmail.com>
2025-09-26 21:37:34 +0900

    env update

--- config/settings.py
+++ config/settings.py
@@ -15,7 +15,7 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = environ.Env()
-environ.Env.read_env(Path(__file__).resolve().parent/'.env')
+environ.Env.read_env(BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
